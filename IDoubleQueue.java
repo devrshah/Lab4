ae0c1a11ce0bc7ddbf0eb27198aa7ac729a547cf
Parthiv Patel <143002114+Parthivp2205@users.noreply.github.com>
2023-10-10 15:42:58 -0400

    Update IDoubleQueue.java

--- IDoubleQueue.java
+++ IDoubleQueue.java
@@ -3,12 +3,13 @@ package com.gradescope.DoubleQueue.code;
 /**IDoubleQueueContract
  *
  *
- *@initialization Ensures: A new double-ended queue is created with no elements and is smaller or equal to maxListSize.
+ *@initialization Ensures: 0 <= queue size <= maxListSize
+ [A new double-ended queue is created with no elements and is of maxListSize.]
  *
- *@defines: None
+ *@defines: queue size <= maxListSize
  *
- *@constraints:  0 < LQueue <= maxListSize AND LQueue != "".
-
+ *@constraints:  0 <= queue size <= maxListSize 
+ * maxListSize > 0
  * [queue is not empty AND is size greater than 0.] AND [The queue can store elements of type Double AND The dequeue operation is only allowed when the queue is not empty.]
  *
  */
@@ -21,9 +22,9 @@ public interface IDoubleQueue
     /**dequeueContract
      *<Description: Removes and returns the element from the front end of the queue.>
      *
-     *@return  [The Double value dequeued from the front of the queue.]
+     *@return  Returns the Double value dequeued from the front of the queue.
      *
-     *@pre [The queue is not empty.]
+     *@pre 0 < queue size [The queue is not empty.]
      *
      *@post [ The front element of the queue is removed, and the size of the queue is reduced by 1] AND [The dequeued element is returned]
      *
@@ -33,9 +34,9 @@ public interface IDoubleQueue
     /**lengthContract
      *<Description: Returns the number of elements currently in the queue.>
      *
-     *@return [The length of the queue as an integer.]
+     *@return Returns the length of the queue as an integer.
      *
-     *@pre None.
+     *@pre queue != NULL.
      *
      *@post [Length of the queue is returned as integer] AND self =#self
      *
@@ -45,11 +46,11 @@ public interface IDoubleQueue
     /**toStringContract
      *<Description: Returns a string representation of the elements in the queue.>
      *
-     *@return [ A string containing the elements in the queue.]
+     *@return Returns a string containing the elements in the queue.
      *
-     *@pre None
+     *@pre queue != NULL.
      *
-     *@post [a string containing the elements in the queue is returned] AND self = #self.
+     *@post [a string containing the elements in the queue is returned as: [#] [#]... [#] ] AND self = #self.
      *
      */
     public String toString();
